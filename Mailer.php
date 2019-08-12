f5da5aac5ff174fc52679472721c528374e1bfa6
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 19:13:26 +0700

    modify author

--- Mailer.php
+++ Mailer.php
@@ -2,7 +2,7 @@
 /**
  * Mailer class file.
  *
- * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2018 Ommu Platform (www.ommu.co)
  * @create date January 12, 2018 15:11 WIB
